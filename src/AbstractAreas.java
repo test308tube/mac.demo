e8890de2583f8f9cd3a6339b7eea7f521a1ccaa2
System Administrator <root@timothy-smitherss-macbook-pro.local>
2013-03-22 08:58:46 -0400

    initial commit

---
+++ src/AbstractAreas.java
@@ -0,0 +1,10 @@
+
+public class AbstractAreas {
+	public static void main(String args []){
+		Figure r = new Rectangle(9,5);
+		Figure t = new Triangle(10,8);
+		
+		System.out.println("Area is " + r.area());
+	}
+
+}
---
+++ src/Test.java
@@ -0,0 +1,22 @@
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(new Test().a());
+
+	}
+	
+	public String a(){
+		
+		return "A";
+	}
+	
+	public int a(int i){
+		
+		return 0;
+	}
+
+}
